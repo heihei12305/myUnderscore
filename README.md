c5b96f1886c3aed5486a393571654c46276ad86d
heihei12305 <2424107459@qq.com>
2019-10-07 11:01:11 +0800

    add extends

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 > ### 这是仿照underscore自己写的函数库
 
-函数里头有大量注释
+函数里头有大量注释,源代码在lib里的[es6-functional](https://github.com/heihei12305/myUnderscore/blob/master/lib/es6-functional.js)中
 
 <br/>
 
@@ -8,10 +8,10 @@
 
 <br/>
 
-测试写在try.js中，通过babel转译成es5，可以用`npm run playground `快速启动
+测试写在try.js中，通过babel转译成es5，可以用`npm run playground `快速启动(当然先`npm install`))
 
 <br/>
 
-即将会有一个gitbook小书来辅助数据库
+即将会有一个gitbook小书来辅助阅读使用函数库,[my-underscore](https://heihei12305.gitbook.io/my-underscore/)
 
 > 未完待续
--- functional-playground/play.js
+++ functional-playground/play.js
@@ -1,12 +1,30 @@
 import _ from '../lib/es6-functional.js'
 
+// let a = ()=>{
+//     // console.log(typeof arg);
+//     let b = [1,2,3];
+//     let d = arguments;
+//     let e = [].slice.call(arguments); 
+//     // let c = arg;
+//     b[1]++;
+// }
+// a(1,2,3);
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(1,2));//3
+
+console.log(_.curry(add)(1)(2));//3
+
 // var array = [1,2,3];
 // _.tap("fun")((it)=>console.log("value is",it))
 // console.log(['1','2','3'].map(_.unary(parseInt)));
 
 // var doPayment = _.once(()=>{
 //     console.log("Payment is done");
-// })x
+// })
 
 // doPayment();
 // doPayment();
@@ -26,7 +44,43 @@ import _ from '../lib/es6-functional.js'
 // console.log(_.flatten(a));
 
 
-let a = [1,2,3,4];
-// console.log(_.reduce(a,(acc,next)=>acc+next,0));
-let b = [];
-console.log(_.zip(a,b,()=>1));
+// let a = [1,2,3,4];
+// // console.log(_.reduce(a,(acc,next)=>acc+next,0));
+// let b = [2,1,2];
+// console.log(_.zip(a,b,(l,r)=>l+r));
+
+
+// let a = {
+//     1:1,
+//     2:2,
+//     3:'a',
+//     length:3
+// }
+
+
+// console.log(_.isArrayLike(a));
+
+
+// _.forEach([1,2,3],(a)=>
+// 	_.tap(a)(()=>{
+// 		console.log(a);
+//     })
+// )
+
+
+
+
+// let fc = _.memoized((n)=>{
+//     if(n===0){
+//         return 1;
+//     }
+//     return n*fc(n-1);
+// })
+// console.time(1)
+// console.log(fc(10));
+// console.timeEnd(1);
+// console.time(2);
+// console.log(fc(11));
+// console.timeEnd(2);
+
+
